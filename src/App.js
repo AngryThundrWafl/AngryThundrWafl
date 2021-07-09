02df8ba29caa6d37c4ac1b3127f0b56689e64412
AngryThundrWafl <pinabrian2015@gmail.com>
2021-07-09 00:55:44 -0600

    Free sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import SideNavPage from ./SideNavPage
+import Sidebar from './SideBar';
+import { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <SideNavPage/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AngryThundrWafl Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Fragment>
+          <Sidebar />
+          </Fragment>
+      </div>
+    </Router>
   );
 }
 
